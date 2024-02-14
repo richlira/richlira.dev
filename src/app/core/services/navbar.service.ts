0eb466e60ff5c7860ed1856062e9849ce3272498
Rich <ricardodelavega@gmail.com>
2024-02-14 00:13:26 -0600

    Rich: update bio class name and add Apple Quicklook link

--- src/app/core/services/navbar.service.ts
+++ src/app/core/services/navbar.service.ts
@@ -7,10 +7,11 @@ export class NavbarService {
         new NavbarItem('https://www.linkedin.com/in/ricardolira/', 'linkedin', 'fa-brands fa-linkedin', '#0a66c2df'),
         new NavbarItem('https://twitter.com/soyrichlira', 'twitter', 'fa-brands fa-twitter', '#006effd2'),
         new NavbarItem('https://www.instagram.com/richlira/', 'instagram', 'fa-brands fa-instagram', '#f84dd0d1'),
-        new NavbarItem('https://github.com/richlira', 'github', 'fa-brands fa-github', '#584df8d1'),
-        new NavbarItem('https://dev.to/richlira', 'devto', 'fa-brands fa-dev secondLine', '#6a6a6a'),
+        new NavbarItem('https://github.com/richlira', 'github', 'fa-brands fa-github', 'red'),
+        new NavbarItem('https://dev.to/richlira', 'devto', 'fa-brands fa-dev secondLine', 'black'),
         new NavbarItem('https://richlira.medium.com', 'medium', 'fa-brands fa-medium secondLine', 'red'),
         new NavbarItem('https://stackoverflow.com/users/5848847/richlira', 'stackoverflow', 'fa-brands fa-stack-overflow secondLine', '#F48024'),
+        new NavbarItem('https://developer.apple.com/augmented-reality/quick-look/', 'apple', 'fa-brands fa-apple secondLine', 'black'),
       ];
 
     constructor() { }
